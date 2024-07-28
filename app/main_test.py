8bbf7dd886d69488775733b9c9bf111004d9670f
Quiwin <qwentzler@orange.fr>
2024-07-28 18:48:26 +0200

    add more test on the sign-encrypt-verify flow

--- app/main_test.py
+++ app/main_test.py
@@ -4,19 +4,18 @@ from app.main import app
 
 client = TestClient(app)
 
+inputs = [
+    {},
+    "{False}",
+    "{1}",
+    "{None}",
+    "toto",
+    {"msg": "Hello World"},
+    {"foo": "foobar", "bar": {"isBar": True}},
+]
 
-@pytest.mark.parametrize(
-    "input",
-    [
-        {},
-        "{False}",
-        "{1}",
-        "{None}",
-        "toto",
-        {"msg": "Hello World"},
-        {"foo": "foobar", "bar": {"isBar": True}},
-    ],
-)
+
+@pytest.mark.parametrize("input", inputs)
 def test_encrypt_decrypt(input):
     encrypt_response = client.post("/encrypt", json=input)
 
@@ -38,18 +37,7 @@ def test_decrypt_invalid():
     assert decrypt_response.json() == {"key": "abcd", "val": "a"}
 
 
-@pytest.mark.parametrize(
-    "input",
-    [
-        {},
-        "{False}",
-        "{1}",
-        "{None}",
-        "toto",
-        {"msg": "Hello World"},
-        {"foo": "foobar", "bar": {"isBar": True}},
-    ],
-)
+@pytest.mark.parametrize("input", inputs)
 def test_sign_verify(input):
     sign_response = client.post("/sign", json=input)
 
@@ -58,3 +46,32 @@ def test_sign_verify(input):
     verify_response = client.post(
         "/verify", json={"signature": sign_response.json(), "data": input}
     )
+
+    assert verify_response.status_code == 200
+
+
+@pytest.mark.parametrize("input", inputs)
+def test_sign_verify_mismatch(input):
+    verify_response = client.post(
+        "/verify", json={"signature": "fuaifziaubf", "data": input}
+    )
+
+    assert verify_response.status_code == 400
+
+
+@pytest.mark.parametrize("input", inputs)
+def test_sign_encrypt_verify(input):
+    sign_response = client.post("/sign", json=input)
+
+    assert sign_response.status_code == 200
+
+    encrypt_response = client.post("/encrypt", json=input)
+
+    assert encrypt_response.status_code == 200
+
+    verify_response = client.post(
+        "/verify",
+        json={"signature": sign_response.json(), "data": encrypt_response.json()},
+    )
+
+    assert verify_response.status_code == 200
