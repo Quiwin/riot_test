23dec863de92100f1f101ab04ac95b637eeb2cb2
Quiwin <qwentzler@orange.fr>
2024-07-28 18:19:44 +0200

    add more test case for sign verify flow and associated fixes

--- app/main.py
+++ app/main.py
@@ -19,7 +19,7 @@ hmac_key = "random_key".encode("utf-8")
 
 @singledispatch
 def _encrypt(payload) -> Any:
-    return crypt.encrypt(str(payload).encode("utf-8"))
+    return crypt.encrypt(json.dumps(payload).encode("utf-8"))
 
 
 @_encrypt.register
@@ -40,16 +40,12 @@ def _(payload: dict) -> Any:
 
 @app.post("/encrypt")
 def encrypt(payload: Any = Body(None)):
-    print(type(payload))
     return _encrypt(payload)
 
 
-@singledispatch
-def _decrypt(payload) -> Any:
+def internal_decrypt(payload):
     try:
-        print(payload)
-        decrypted = crypt.decrypt(payload.encode("utf-8"))
-        print(decrypted)
+        decrypted = crypt.decrypt(json.dumps(payload).encode("utf-8"))
         return json.loads(decrypted)
     except (InvalidException, UnicodeDecodeError):
         return payload
@@ -57,11 +53,16 @@ def _decrypt(payload) -> Any:
         return decrypted
 
 
+@singledispatch
+def _decrypt(payload) -> Any:
+    return internal_decrypt(payload)
+
+
 @_decrypt.register
 def _(payload: list):
     res = []
     for value in payload:
-        res += _decrypt(value)
+        res += internal_decrypt(value)
     return res
 
 
@@ -69,13 +70,12 @@ def _(payload: list):
 def _(payload: dict):
     res = {}
     for key, value in payload.items():
-        res[key] = _decrypt(value)
+        res[key] = internal_decrypt(value)
     return res
 
 
 @app.post("/decrypt")
 def decrypt(payload: Any = Body(None)):
-    print(type(payload))
     return _decrypt(payload)
 
 
@@ -85,15 +85,12 @@ def _sign(payload: bytes):
 
 @app.post("/sign")
 def sign(payload: Any = Body(None)):
-    print(payload)
-    print(json.dumps(payload))
     res = _sign(json.dumps(payload).encode("utf-8"))
     return res
 
 
 @app.post("/verify")
 def verify(signature: Annotated[str, Body()], data: Annotated[Any, Body()]):
-    print(any)
     msg = json.dumps(decrypt(data))
     digest = _sign(msg.encode("utf-8"))
     if hmac.compare_digest(digest, signature.encode("utf-8")):
--- app/main_test.py
+++ app/main_test.py
@@ -22,7 +22,6 @@ def test_encrypt_decrypt(input):
 
     assert encrypt_response.status_code == 200
 
-    print(encrypt_response.json())
     decrypt_response = client.post("/decrypt", json=encrypt_response.json())
 
     assert decrypt_response.status_code == 200
@@ -39,13 +38,23 @@ def test_decrypt_invalid():
     assert decrypt_response.json() == {"key": "abcd", "val": "a"}
 
 
-def test_sign_verify():
-    input = '{"string"}'
+@pytest.mark.parametrize(
+    "input",
+    [
+        {},
+        "{False}",
+        "{1}",
+        "{None}",
+        "toto",
+        {"msg": "Hello World"},
+        {"foo": "foobar", "bar": {"isBar": True}},
+    ],
+)
+def test_sign_verify(input):
     sign_response = client.post("/sign", json=input)
 
     assert sign_response.status_code == 200
 
-    print(sign_response.content)
     verify_response = client.post(
         "/verify", json={"signature": sign_response.json(), "data": input}
     )
